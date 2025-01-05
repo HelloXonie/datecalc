8f55cf2cced555d99d4d5d2bbf990d2d13ebe566
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2025-01-05 12:27:36 -0800

    adding in a progress bar

--- datecalc.py
+++ datecalc.py
@@ -1,11 +1,26 @@
 #!/bin/python3
 
 from datetime import date
+from os_sys.progress import bar
+
+#get the current date and subtract from the future date
 today = date.today()
-future = date(2028,01,01)
+future = date(2028,12,20)
 
 remaining_days = str(future - today)
 
 print (remaining_days)
+
+#progress bar
+bar = Bar ('Processing', max=20)
+for i in range(20):
+    bar.next()
+bar.finish()
+
+print (bar)
+
+
+
+
 
 
