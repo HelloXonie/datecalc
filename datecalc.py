3ebc9982cd444abc3ddb24d9b9e5ea8dd1c5bd6b
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2025-01-06 21:26:32 -0800

    made changes from tdqm to progressbar for module. Bar still printing to terminal. Testing to print it to a log file instead

--- datecalc.py
+++ datecalc.py
@@ -1,8 +1,12 @@
 #!/bin/python3
 
+import sys
+import os
 import time
+import progressbar
+from io import StringIO
 from datetime import date
-from tqdm import tqdm
+#from tqdm import tqdm
 from time import sleep
 
 #get the current date and subtract from the future date
@@ -13,8 +17,26 @@ remaining_days = str(future - today)
 
 print (remaining_days)
 
-for i in tqdm(range(10)):
-    time.sleep (0.1)
+print (progressbar.__version__)
+#for i in tqdm(range(10)):
+#    time.sleep (0.1)
+
+output = StringIO()
+sys.stdout = output
+
+bar = progressbar.ProgressBar(maxval=100, \
+    widgets = [progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+bar.start()
+for i in (range(20), file = log_file):
+    bar.update(i+1)
+    sleep(0.1)
+bar.finish()
+log_file.flush()
+
+#sys.stdout = sys.__stdout__
+
+#print(output.getvalue())
+
 
 
 
